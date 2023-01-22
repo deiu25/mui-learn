059ed25f8e2f13d6cb56a864be5a9bd5df53e718
deiu25 <d3iu25@gmail.com>
2023-01-22 20:56:14 +0200

    Button

--- src/Test.js
+++ src/Test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Typography } from "@mui/material";
+import { Button } from "@mui/material";
 
 const Test = () => {
   return (
@@ -7,6 +8,33 @@ const Test = () => {
       <Typography variant="h5" sx={{ color: "red" }}>
         Hello World
       </Typography>
+      <Button
+        onClick={() => alert("Button Clicked")}
+        color="success"
+        sx={{ margin: 3 }}
+        size="large"
+        variant="contained"
+      >
+        First
+      </Button>
+      <Button
+        onClick={() => alert("Button Clicked")}
+        color="error"
+        sx={{ margin: 3 }}
+        size="medium"
+        variant="outlined"
+      >
+        Second
+      </Button>
+      <Button
+        onClick={() => alert("Button Clicked")}
+        color="info"
+        sx={{ margin: 3 }}
+        size="small"
+        variant="text"
+      >
+        Third
+      </Button>
     </div>
   );
 };
